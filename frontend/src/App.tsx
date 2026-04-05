dbe4023c6c43511be6a91eb27b03e5219697a28e
Andrew flensburg <sanskar379tiwari@gmail.com>
2026-04-05 09:12:41 +0530

    Remove unused pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AppShell } from './components/layout/AppShell'
 import { Landing } from './pages/Landing'
-import { Events } from './pages/Events'
 import { EventDetail } from './pages/EventDetail'
-import { Register } from './pages/Register'
-import { Confirmation } from './pages/Confirmation'
 
 export default function App() {
   return (
@@ -12,10 +9,7 @@ export default function App() {
       <Routes>
         <Route element={<AppShell />}>
           <Route path="/" element={<Landing />} />
-          <Route path="/events" element={<Events />} />
           <Route path="/events/:slug" element={<EventDetail />} />
-          <Route path="/events/:slug/register" element={<Register />} />
-          <Route path="/events/:slug/confirmation" element={<Confirmation />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
